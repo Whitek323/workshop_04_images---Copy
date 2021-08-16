c26cb9d6c793f27cb9ba8aae88ee8d80d1b07d33
Whitek323 <whitek8327@gmail.com>
2021-08-16 12:19:28 +0700

    first commit

---
+++ src/main/java/com/kittikon/workshop_04_images/Workshop04ImagesApplication.java
@@ -0,0 +1,13 @@
+package com.kittikon.workshop_04_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop04ImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop04ImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kittikon/workshop_04_images/Workshop04ImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kittikon.workshop_04_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop04ImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
